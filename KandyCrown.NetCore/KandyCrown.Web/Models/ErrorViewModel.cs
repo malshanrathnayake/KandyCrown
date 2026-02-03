bbc3982d02e0722f36c16ae44e5ca0fe57c7ff75
Malshan Rathnayake <malshan.rathnayake@iits.biz>
2026-02-03 20:50:26 +0530

    initial web

---
+++ KandyCrown.NetCore/KandyCrown.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KandyCrown.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
